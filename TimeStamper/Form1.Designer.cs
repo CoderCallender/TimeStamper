448663a968862c83bfea4e7aa6d2f52834a5b817
CoderCallender <42542281+CoderCallender@users.noreply.github.com>
2019-09-21 09:14:03 +0100

    Elaspsed time calc

--- TimeStamper/Form1.Designer.cs
+++ TimeStamper/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.StartTimeTextBox = new System.Windows.Forms.TextBox();
+            this.EndTimeTextBox = new System.Windows.Forms.TextBox();
+            this.calc_button = new System.Windows.Forms.Button();
+            this.ResultTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Start";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "End";
+            // 
+            // StartTimeTextBox
+            // 
+            this.StartTimeTextBox.Location = new System.Drawing.Point(100, 44);
+            this.StartTimeTextBox.Name = "StartTimeTextBox";
+            this.StartTimeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.StartTimeTextBox.TabIndex = 2;
+            // 
+            // EndTimeTextBox
+            // 
+            this.EndTimeTextBox.Location = new System.Drawing.Point(100, 107);
+            this.EndTimeTextBox.Name = "EndTimeTextBox";
+            this.EndTimeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.EndTimeTextBox.TabIndex = 4;
+            this.EndTimeTextBox.TextChanged += new System.EventHandler(this.EndTimeTextBox_TextChanged);
+            // 
+            // calc_button
+            // 
+            this.calc_button.Location = new System.Drawing.Point(19, 174);
+            this.calc_button.Name = "calc_button";
+            this.calc_button.Size = new System.Drawing.Size(75, 23);
+            this.calc_button.TabIndex = 8;
+            this.calc_button.Text = "Calculate";
+            this.calc_button.UseVisualStyleBackColor = true;
+            this.calc_button.Click += new System.EventHandler(this.calc_button_Click);
+            // 
+            // ResultTextBox
+            // 
+            this.ResultTextBox.Location = new System.Drawing.Point(100, 176);
+            this.ResultTextBox.Name = "ResultTextBox";
+            this.ResultTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ResultTextBox.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(268, 261);
+            this.Controls.Add(this.ResultTextBox);
+            this.Controls.Add(this.calc_button);
+            this.Controls.Add(this.EndTimeTextBox);
+            this.Controls.Add(this.StartTimeTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Elapsed Time Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox StartTimeTextBox;
+        private System.Windows.Forms.TextBox EndTimeTextBox;
+        private System.Windows.Forms.Button calc_button;
+        private System.Windows.Forms.TextBox ResultTextBox;
     }
 }
 
--- TimeStamper/Form1.cs
+++ TimeStamper/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,88 @@ namespace TimeStamper
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calc_button_Click(object sender, EventArgs e)
+        {
+            string start_time = StartTimeTextBox.Text;
+            string end_time = EndTimeTextBox.Text;
+            
+            //strip into usable numbers
+            string[] start_split = start_time.Split(':','.');
+            string[] end_split = end_time.Split(':','.');
+
+
+            int start_hour = int.Parse(start_split[0]);
+            int start_min = int.Parse(start_split[1]);
+       //     int start_sec = int.Parse(start_split[2]);
+
+            Debug.WriteLine("start");
+            Debug.WriteLine(start_hour);
+            Debug.WriteLine(start_min);
+        //    Debug.WriteLine(start_sec);
+
+            int end_hour = int.Parse(end_split[0]);
+            int end_min = int.Parse(end_split[1]);
+        //    int end_sec = int.Parse(end_split[2]);
+
+            Debug.WriteLine("end");
+            Debug.WriteLine(end_hour);
+            Debug.WriteLine(end_min);
+          //  Debug.WriteLine(end_sec);
+
+            //always assume that the end is after the start for now
+            int hour = 0;
+            int min = 0;
+
+            //do this if it has passed midnight
+            if (start_hour > end_hour)
+            {
+                hour = start_hour + end_hour;
+            }
+
+            else
+            {
+                hour = end_hour - start_hour;
+            }
+
+            if(start_min <= end_min)
+            {
+                min = end_min - start_min;
+            }
+
+            else
+            {
+                hour = hour - 1;
+                min = (end_min - start_min) + 60;
+            }
+            Debug.WriteLine("result");
+            Debug.WriteLine(hour);
+            ResultTextBox.Text = hour.ToString() + ":" + min.ToString(); ;
+
+            //  
+
+        }
+
+        private void EndTimeTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                calc_button.PerformClick();
+                // these last two lines will stop the beep sound
+                e.SuppressKeyPress = true;
+                e.Handled = true;
+            }
+        }
+
+        private void EndTimeTextBox_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
